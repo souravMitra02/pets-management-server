c8b0f168f2606f2a189c41d62bdbf35aa48c90f3
Analytics Dashboard <developer@analytics-dashboard.local>
2026-03-26 11:48:33 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +11,48 @@ app.use(express.json());
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yiklsv0.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+
+
+
+
+
+
+
+
+
+
+
+
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+ 
+    // await client.close();
+  }
+}
+
+
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send("pets server is running....");
 });
@@ -16,3 +60,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+run().catch(console.dir);
